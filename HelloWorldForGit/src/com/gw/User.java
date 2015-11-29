58e8c5b0ef19b2a43a8d9ebe983d6e4b15bdbde8
hilldo <hilldo_lv@163.com>
2015-11-29 15:07:15 +0800

    update User.java

--- HelloWorldForGit/src/com/gw/User.java
+++ HelloWorldForGit/src/com/gw/User.java
@@ -3,6 +3,15 @@ package com.gw;
 public class User {
 
 	private String name;
+	private Integer age;
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
 
 	public String getName() {
 		return name;
