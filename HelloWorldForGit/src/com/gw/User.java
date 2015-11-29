c380546e178a8f1163a92eaf3ab84f65c2862465
hilldo <hilldo_lv@163.com>
2015-11-29 15:05:54 +0800

    add User class

---
+++ HelloWorldForGit/src/com/gw/User.java
@@ -0,0 +1,15 @@
+package com.gw;
+
+public class User {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
